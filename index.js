75a4ddff318c6f85e549ff24cab7c85f3054e4d8
FehrenbachMark <fehrenbachmark@gmail.com>
2025-07-28 19:57:17 -0400

    swapped to puppeteer instead of axios+cheerio, rest is mostly the same except styling. Now caching results every Monday, to reduce site pings and champs don't change often

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const PORT = process.env.PORT || 3000;
-const axios = require('axios');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const express = require('express');
 const moment = require('moment');
 require('dotenv').config();
@@ -14,68 +13,83 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-const url = 'https://www.ufc.com/athletes'
-axios(url)
-  .then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    // declares empty arrays to store the data that we'll scrape
-    const fighters = [];
-    const fighterInfo = [];
-    const fighterImages = [];
-    const fighterLinks = [];
-    const lastFight = [];
-    // finds the class of the element that we want to scrape from ufc dom and pulls the fighter info from that element.
-    $('.ath-n__name', html).each(function () {
 
-      const athletes = $(this).text().split('\n').join('').split("'").join('').split(' ').join('');
-      const url = $(this).find('a').attr('href');
-      fighters.push(athletes.split(/(?=[A-Z])/).join(' '));
-      fighterLinks.push("https://ufc.com" + url);
-    });
-    // console.log(fighters);
-
-    // finds the fighters images and adds them to the fighterImages array
-    $('.c-embedded-single-media__item', html).each(function () {
-      const image = $(this).find('img').attr('src');
-      // add images to fighterImagers array
-      fighterImages.push(image);
-    })
-    // console.log(fighterImages);
-    // finds the fighters info and adds them to the fighterInfo array
-    $('.ath--winfo', html).each(function () {
-      const info = $(this).text().split('\n').join('').split("'").join('').split(' ').join('');
-      let newInfo = info.split(/(?=[A-Z])/).join(' ');
-      let formattedFighterInfo = newInfo.replace(/\d+/, " $&").split(')')[0];
-      fighterInfo.push(formattedFighterInfo);
-    })
-    // console.log(fighterInfo);
-
-    // finds the fighters last fight and adds them to the lastFight array
-    $('.ath-lf .view-fighter-last-fight', html).each(function () {
-      const lastFightInfo = $(this).find('.views-row').text();
-      lastFight.push(lastFightInfo);      
-    })
-    // console.log(lastFight);
-
-    // removes every other element in the array for formatting purposes
-    for (let i = 0; i < lastFight.length; i++) {
-      if (i % 2 !== 0) {
-        lastFight.splice(i, 2);
-      }
-    }
-
-    // console.log(lastFight);
-
-
-    app.get('/', (req, res) => {
-      res.render('index', { fighters: fighters, fighterImages: fighterImages, fighterLinks: fighterLinks, fighterInfo: fighterInfo, moment: moment, lastFight: lastFight });
-    });
-
-  })
-  .catch(error => {
-    console.log(error);
+// Scrape fighters using Puppeteer
+async function scrapeFighters() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('https://www.ufc.com/athletes', {
+    waitUntil: 'networkidle2',
+    timeout: 60000
   });
+  await page.waitForSelector('.ath-n__name', { timeout: 20000 });
+  const fighters = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.ath-n__name')).map(el => el.innerText.trim());
+  });
+  const fighterImages = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.atm-thumbnail img')).map(el => el.src.trim());
+  });
+  const lastFight = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.view-fighter-last-fight'))
+      .filter((_, i) => i % 2 === 0)
+      .map(el => el.innerText.trim());
+  });
+  const fighterLinks = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.ath-n__name a')).map(el => el.href.trim());
+  });
+  const fighterInfo = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.ath--winfo')).map(el => el.innerText.trim());
+  });
+
+
+  await browser.close();
+  return { fighters, fighterImages, lastFight, fighterLinks, fighterInfo };
+}
+
+// In-memory cache for weekly scraping
+let cachedData = null;
+let lastScrapeDate = null;
+
+app.get('/', async (req, res) => {
+  const today = moment().startOf('day');
+  const isMonday = today.isoWeekday() === 1; // Monday = 1
+  let useCache = false;
+
+  if (cachedData && lastScrapeDate && lastScrapeDate.isSame(today, 'day')) {
+    useCache = true;
+  }
+
+  if (!useCache && isMonday) {
+    try {
+      cachedData = await scrapeFighters();
+      lastScrapeDate = today;
+      console.log('Scraped new data on Monday.');
+    } catch (err) {
+      console.log('Puppeteer scrape error:', err.message);
+      // fallback to cache if available
+      if (!cachedData) cachedData = { fighters: [], fighterImages: [], lastFight: [], fighterLinks: [], fighterInfo: [] };
+    }
+  } else if (!cachedData) {
+    // If cache is empty and not Monday, scrape once
+    try {
+      cachedData = await scrapeFighters();
+      lastScrapeDate = today;
+      console.log('Scraped new data (first run or cache empty).');
+    } catch (err) {
+      console.log('Puppeteer scrape error:', err.message);
+      cachedData = { fighters: [], fighterImages: [], lastFight: [], fighterLinks: [], fighterInfo: [] };
+    }
+  }
+
+  res.render('index', {
+    fighters: cachedData.fighters,
+    fighterImages: cachedData.fighterImages,
+    fighterLinks: cachedData.fighterLinks,
+    fighterInfo: cachedData.fighterInfo,
+    moment: moment,
+    lastFight: cachedData.lastFight
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
 }).on('error', (err) => {
